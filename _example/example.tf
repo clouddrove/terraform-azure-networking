72fdb4e2c720f69646f297a0e57e37eeb925892e
Ola <olaitanfalolu@gmail.com>
2022-04-19 12:13:44 +0100

    initial commit

--- _example/example.tf
+++ _example/example.tf
@@ -27,3 +27,16 @@ module "vnet" {
   subnet_prefixes     = ["10.0.1.0/24"]
   enable_ddos_pp      = true
 }
+
+module "route_table" {
+  source              = "../"
+  name                = "route-table"
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  routes = [
+    { name = "route1", address_prefix = "10.0.1.0/24", next_hop_type = "VirtualAppliance", next_hop_in_ip_address = "10.0.0.4" },
+    { name = "route2", address_prefix = "10.0.1.0/24", next_hop_type = "VirtualAppliance", next_hop_in_ip_address = "10.0.0.4" },
+    { name = "route3", address_prefix = "0.0.0.0/0", next_hop_type = "Internet" }
+  ]
+  disable_bgp_route_propagation = false
+}
