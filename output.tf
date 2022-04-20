9ad73d49a9991cc455aad8f7c034742f6a50adc8
Ola <olaitanfalolu@gmail.com>
2022-04-20 16:02:46 +0100

    network resoucre

--- output.tf
+++ output.tf
@@ -1,24 +1,41 @@
-output "vnet_id" {
-  description = "The id of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.*.id
-}
 
-output "vnet_name" {
-  description = "The name of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.*.name
-}
 
-output "vnet_location" {
-  description = "The location of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.*.location
-}
+# output "virtual_network_name" {
+#   description = "The name of the virtual network"
+#   value       = element(concat(azurerm_virtual_network.vnet.*.name, [""]), 0)
+# }
 
-output "vnet_address_space" {
-  description = "The address space of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.*.address_space
-}
+# output "virtual_network_id" {
+#   description = "The id of the virtual network"
+#   value       = element(concat(azurerm_virtual_network.vnet.*.id, [""]), 0)
+# }
 
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the newly created vNet"
-  value       = azurerm_subnet.subnet.*.id
-}
+# output "virtual_network_address_space" {
+#   description = "List of address spaces that are used the virtual network."
+#   value       = element(coalescelist(azurerm_virtual_network.vnet.*.address_space, [""]), 0)
+# }
+
+# output "subnet_ids" {
+#   description = "List of IDs of subnets"
+#   value       = flatten(concat([for s in azurerm_subnet.snet : s.id], [var.gateway_subnet_address_prefix != null ? azurerm_subnet.gw_snet.0.id : null], [var.firewall_subnet_address_prefix != null ? azurerm_subnet.fw-snet.0.id : null]))
+# }
+
+# output "subnet_address_prefixes" {
+#   description = "List of address prefix for subnets"
+#   value       = flatten(concat([for s in azurerm_subnet.snet : s.address_prefix], [var.gateway_subnet_address_prefix != null ? azurerm_subnet.gw_snet.0.address_prefixes : null], [var.firewall_subnet_address_prefix != null ? azurerm_subnet.fw-snet.0.address_prefixes : null]))
+# }
+
+# output "network_security_group_ids" {
+#   description = "List of Network security groups and ids"
+#   value       = [for n in azurerm_network_security_group.nsg : n.id]
+# }
+
+# output "ddos_protection_plan" {
+#   description = "Ddos protection plan details"
+#   value       = var.create_ddos_plan ? element(concat(azurerm_network_ddos_protection_plan.ddos.*.id, [""]), 0) : null
+# }
+
+# # output "network_watcher_id" {
+# #   description = "ID of Network Watcher"
+# #   value       = var.create_network_watcher != false ? element(concat(azurerm_network_watcher.nwatcher.*.id, [""]), 0) : null
+# # }
--- variable.tf
+++ variable.tf
@@ -1,123 +1,97 @@
-#Module      : LABEL
-#Description : Terraform label module variables.
-variable "name" {
-  type        = string
-  default     = ""
-  description = "Name  (e.g. `app` or `cluster`)."
-}
-
-variable "repository" {
-  type        = string
-  default     = "https://github.com/clouddrove/terraform-azure-virtual-network"
-  description = "Terraform current module repo"
-
-  validation {
-    # regex(...) fails if it cannot find a match
-    condition     = can(regex("^https://", var.repository))
-    error_message = "The module-repo value must be a valid Git repo link."
-  }
-}
-
-variable "environment" {
-  type        = string
-  default     = ""
-  description = "Environment (e.g. `prod`, `dev`, `staging`)."
-}
-
-variable "label_order" {
-  type        = list(any)
-  default     = []
-  description = "Label order, e.g. `name`,`application`."
-}
-
-variable "attributes" {
-  type        = list(any)
-  default     = []
-  description = "Additional attributes (e.g. `1`)."
-}
-
-variable "delimiter" {
-  type        = string
-  default     = "-"
-  description = "Delimiter to be used between `organization`, `environment`, `name` and `attributes`."
-}
-
-variable "tags" {
-  type        = map(any)
-  default     = {}
-  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
-}
-
-variable "managedby" {
-  type        = string
-  default     = "hello@clouddrove.com"
-  description = "ManagedBy, eg 'CloudDrove'."
-}
-
-variable "enable" {
-  type        = bool
+variable "create_resource_group" {
+  description = "Whether to create resource group and use it for all networking resources"
   default     = true
-  description = "Flag to control the module creation"
 }
 
 variable "resource_group_name" {
-  type        = string
+  description = "A container that holds related resources for an Azure solution"
   default     = ""
-  description = "The name of an existing resource group to be imported."
 }
 
 variable "location" {
-  type        = string
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
   default     = ""
-  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
 }
 
-variable "address_space" {
-  type        = string
+variable "vnetwork_name" {
+  description = "Name of your Azure Virtual Network"
   default     = ""
-  description = "The address space that is used by the virtual network."
 }
 
-variable "address_spaces" {
-  type        = list(string)
-  default     = []
-  description = "The list of the address spaces that is used by the virtual network."
+variable "vnet_address_space" {
+  description = "The address space to be used for the Azure virtual network."
+  default     = ""
 }
 
-# If no values specified, this defaults to Azure DNS 
-variable "dns_servers" {
-  type        = list(string)
-  default     = []
-  description = "The DNS servers to be used with vNet."
-}
-
-variable "subnet_prefixes" {
-  type        = list(string)
-  default     = []
-  description = "The address prefix to use for the subnet."
-}
-
-variable "subnet_names" {
-  type        = list(string)
-  default     = []
-  description = "A list of public subnets inside the vNet."
-}
-
-
-variable "subnet_enforce_private_link_endpoint_network_policies" {
-  type        = map(bool)
-  default     = {}
-  description = "A map with key (string) `subnet name`, value (bool) `true` or `false` to indicate enable or disable network policies for the private link endpoint on the subnet. Default value is false."
-}
-
-variable "subnet_service_endpoints" {
-  type        = map(list(string))
-  default     = {}
-  description = "A map with key (string) `subnet name`, value (list(string)) to indicate enabled service endpoints on the subnet. Default value is []."
-}
-
-variable "enable_ddos_pp" {
-  type        = bool
+variable "create_ddos_plan" {
+  description = "Create an ddos plan - Default is false"
   default     = false
-  description = "Flag to control the resource creation"
+}
+
+variable "dns_servers" {
+  description = "List of dns servers to use for virtual network"
+  default     = []
+}
+
+variable "ddos_plan_name" {
+  description = "The name of AzureNetwork DDoS Protection Plan"
+  default     = "azureddosplan01"
+}
+
+variable "create_network_watcher" {
+  description = "Controls if Network Watcher resources should be created for the Azure subscription"
+  default     = true
+}
+
+variable "subnets" {
+  description = "For each subnet, create an object that contain fields"
+  default     = {}
+}
+
+variable "gateway_subnet_address_prefix" {
+  description = "The address prefix to use for the gateway subnet"
+  default     = null
+}
+
+variable "firewall_subnet_address_prefix" {
+  description = "The address prefix to use for the Firewall subnet"
+  default     = null
+}
+
+variable "firewall_service_endpoints" {
+  description = "Service endpoints to add to the firewall subnet"
+  type        = list(string)
+  default = [
+    "Microsoft.AzureActiveDirectory",
+    "Microsoft.AzureCosmosDB",
+    "Microsoft.EventHub",
+    "Microsoft.KeyVault",
+    "Microsoft.ServiceBus",
+    "Microsoft.Sql",
+    "Microsoft.Storage",
+  ]
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the route table."
+}
+
+variable "routes" {
+  type        = list(map(string))
+  default     = []
+  description = "List of objects that represent the configuration of each route."
+  /*ROUTES = [{ name = "", address_prefix = "", next_hop_type = "", next_hop_in_ip_address = "" }]*/
+}
+
+variable "disable_bgp_route_propagation" {
+  type        = bool
+  default     = true
+  description = "Boolean flag which controls propagation of routes learned by BGP on that route table."
 }
