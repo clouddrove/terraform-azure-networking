e716508fc44b81bf0d284765d8fd6c9682715695
mahesh yadav <mahesh.yadav@clouddrove.com>
2022-12-09 22:53:25 +0530

    fix name type

--- _example/example.tf
+++ _example/example.tf
@@ -6,24 +6,22 @@ module "resource_group" {
   source  = "clouddrove/resource-group/azure"
   version = "1.0.0"
 
-  environment = "test"
-  label_order = ["name", "environment", ]
-
-  name     = "example-resource-group"
+  name     = "app"
+  environment = "env"
+  label_order = ["environment", "name", ]
   location = "North Europe"
 }
 
 module "vnet" {
   source = "../"
 
-  environment = "test"
+  name                = "app"
+  environment = "env"
   label_order = ["name", "environment"]
-
-  name                = "example"
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
   address_space       = "10.0.0.0/16"
-  enable_ddos_pp      = true
+  enable_ddos_pp      = false
 
   #subnet
   subnet_names                  = ["subnet1", "subnet2"]
